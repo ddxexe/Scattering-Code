5ab4deea42477c1b977599b28df05ddb1b352ddd
ddxexe <jwneu97@gmail.com>
2021-05-25 14:52:36 -0400

    Update adj_coord.m
    Changed coordinate calculations

--- adj_coord.m
+++ adj_coord.m
@@ -12,19 +12,23 @@ x0 = sin(ang_theta)*cos(ang_phi);
 y0 = sin(ang_theta)*sin(ang_phi);
 z0 = cos(ang_theta);
 
-x = x0*cos(ang_theta)*cos(ang_phi) - y0*sin(ang_phi) + z0*sin(ang_theta)*cos(ang_phi);
-y = x0*cos(ang_theta)*sin(ang_phi) + y0*cos(ang_phi) + z0*sin(ang_theta)*cos(ang_phi);
-z = -x0*sin(ang_theta) + z0*cos(ang_theta);
+x1 = x0*cos(ang_theta)*cos(ang_phi) - y0*sin(ang_phi) + z0*sin(ang_theta)*cos(ang_phi);
+y1 = x0*cos(ang_theta)*sin(ang_phi) + y0*cos(ang_phi) + z0*sin(ang_theta)*cos(ang_phi);
+z1 = -x0*sin(ang_theta) + z0*cos(ang_theta);
 
-ang_theta = acos(z);
-ang_phi = atan(x/y);
+ang_theta = acos(z1);
+ang_phi = atan(x1/y1);
 
 if ang_phi < 0,
     ang_phi = ang_phi + pi;
 endif
 
-angle_data_2(1) = ang_phi;
-angle_data_2(2) = ang_theta;
+
+angle_date_2(1) = x + x1;
+angle_data_2(2) = y + y1;
+angle_data_2(3) = z + z1;
+angle_data_2(4) = ang_phi;
+angle_data_2(5) = ang_theta;
 
 result = angle_data_2;
 end
